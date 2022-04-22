869654fa0ab7cac7cf670d5b6ea592d257a76cec
MarMar Tahery <85257802+mtahery20@users.noreply.github.com>
2022-04-22 00:22:34 -0700

    Styling

--- execution.php
+++ execution.php
@@ -22,7 +22,6 @@ echo "<h1><i> Welcome,&nbsp" . $first . ".</i></h1>";
 </head>
 
 <body>
-    <h2><a href="part3.php">Back to Part 3 Queries Page</a></h2>
     <?php
         if(isset($_POST['button1'])){
             echo 'button 1 worked!';
@@ -37,6 +36,7 @@ echo "<h1><i> Welcome,&nbsp" . $first . ".</i></h1>";
             echo 'button 4 worked!';
         }
         if(isset($_POST['button5'])){
+            echo "<p><h3> Lists user pairs ('X', 'Y') such that they have at least one common hobby. </h3></p>";
             $qry5 = "SELECT DISTINCT user, user2 FROM
             (SELECT hobbyId AS hID, user, result.user2
             FROM user_hobbies
@@ -50,28 +50,38 @@ echo "<h1><i> Welcome,&nbsp" . $first . ".</i></h1>";
             if ($sql5->num_rows > 0) {
                 echo "<br>";
                 while ($row5 = $sql5->fetch_assoc()) {
+                    echo "<h4>";
                     echo "('" . $row5["user"] . "',";
                     echo " '" . $row5["user2"] . "')";
                     echo "<br>";
+                    echo "</h4>";
                 } 
             }else {
+                echo "<h4>";
                 echo ("Returned no results :( ");
+                echo "</h4>";
               }
         }
         if(isset($_POST['button6'])){
+            echo "<p><h3> Displays all the users who have yet to post a blog. </p></h3>";
             $qry6 = "SELECT username FROM comp440.user 
             LEFT JOIN comp440.blogs ON user.username=blogs.ownerUsername WHERE blogs.blogId is NULL";
             $sql6 = mysqli_query($conn_comp440, $qry6);
             if ($sql6->num_rows > 0) {
                 echo "<br>";
                 while ($row6 = $sql6->fetch_assoc()) {
+                    echo "<h4>";
                     echo " '" . $row6["username"] . "' ";
+                    echo "</h4>";
                   }
                 } else {
-                  echo ("Returned no results :( ");
+                    echo "<h4>";
+                    echo ("Returned no results :( ");
+                    echo "</h4>";
                 }
             }
         if(isset($_POST['button7'])){
+            echo "<p><h3> Displays all the users who have yet to post a comment. </p><h3>";
             $qry7 = "SELECT username
             FROM comp440.user
             LEFT JOIN comp440.comments
@@ -82,13 +92,18 @@ echo "<h1><i> Welcome,&nbsp" . $first . ".</i></h1>";
             if ($sql7->num_rows > 0) {
                 echo "<br>";
                 while ($row7 = $sql7->fetch_assoc()) {
+                    echo "<h4>";
                     echo " '" . $row7["username"] . "' ";
+                    echo "</h4>";
                 }
             } else {
+                echo "<h4>";
                 echo ("Returned no results :(");
+                echo "</h4>";
             }
         }
         if(isset($_POST['button8'])){
+            echo "<p><h3> Displays all the users who have only posted negative comments. </p><h3>";
             $qry8 = "SELECT ownerUsername 
             FROM (SELECT ownerUsername, SUM(sentiment) AS sum 
             FROM comments GROUP BY ownerUsername) AS result WHERE sum = 0";
@@ -96,13 +111,18 @@ echo "<h1><i> Welcome,&nbsp" . $first . ".</i></h1>";
             if ($sql8->num_rows > 0) {
                 echo "<br>";
                 while ($row8 = $sql8->fetch_assoc()) {
-                  echo " '" . $row8["ownerUsername"] . "' ";
+                    echo "<h4>";
+                    echo " '" . $row8["ownerUsername"] . "' ";
+                    echo "</h4>";
                 }
               } else {
+                echo "<h4>";
                 echo ("Returned no results :( ");
+                echo "</h4>";
               }
         } 
         if(isset($_POST['button9'])){
+            echo "<p><h3> Displays users who have yet to receive a negative comment on any of their blogs. </p><h3>";
             $qry9 = "SELECT ownerUsername FROM blogs
                     WHERE ownerUsername NOT IN 
                     (SELECT ownerUsername FROM blogs
@@ -118,13 +138,19 @@ echo "<h1><i> Welcome,&nbsp" . $first . ".</i></h1>";
             if ($sql9->num_rows > 0) {
                 echo "<br>";
                 while ($row9 = $sql9->fetch_assoc()) {
-                  echo " '" . $row9["ownerUsername"] . "' ";
+                    echo "<h4>";
+                    echo " '" . $row9["ownerUsername"] . "' ";
+                    echo "</h4>";
                 }
             } else {
+                echo "<h4>";
                 echo ("Returned no results :(");
+                echo "</h4>";
             }
         }
     ?>
+    <br>
+    <h2><a href="part3.php">Back to Part 3 Queries Page</a></h2>
 </body>
 
 </html>
